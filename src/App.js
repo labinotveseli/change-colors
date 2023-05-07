8422959bba08ec719a6340e3e6ed5d46bded9054
Labinot Veseli <labinotveseli1@gmail.com>
2023-05-07 23:32:12 +0200

    Adding React App Color Changer

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import Square from "./Square";
+import Input from "./Input";
+import { useState } from "react";
+
+function App() {
+  const [colorValue, setColorValue] = useState("");
+  const [hexValue, setHexValue] = useState("");
+  const [isDarkText, setIsDarkText] = useState(true);
+
+  return (
+    <div className="App">
+      <h1 className="h1">
+        React APP for changing colors and receiving the HEX code
+      </h1>
+      <Square
+        colorValue={colorValue}
+        hexValue={hexValue}
+        isDarkText={isDarkText}
+      />
+      <Input
+        colorValue={colorValue}
+        setColorValue={setColorValue}
+        setHexValue={setHexValue}
+        isDarkText={isDarkText}
+        setIsDarkText={setIsDarkText}
+      />
+    </div>
+  );
+}
+
+export default App;
